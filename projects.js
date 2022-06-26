abf0ce8b63ff2d08f225b224a6437ea17c7a88ea
John Niyontwali <nijohn0006@gmail.com>
2022-06-26 17:07:11 +0200

    added the chicken-order app to my project section

--- projects.js
+++ projects.js
@@ -8,6 +8,9 @@ import epiclib1 from "./public/assets/projects/epiclib1.jpg";
 import epiclib2 from "./public/assets/projects/epiclib2.jpg";
 import portfolio1 from "./public/assets/projects/portfolio1.jpg";
 import portfolio2 from "./public/assets/projects/portfolio2.jpg";
+import chicken1 from "./public/assets/projects/chicken1.jpg";
+import chicken2 from "./public/assets/projects/chicken2.jpg";
+
 
 export const projects = [
   {
@@ -31,7 +34,7 @@ export const projects = [
     image2: phantom2,
     link: "https://phantom-frontend-elites.herokuapp.com/",
     description:
-      "This is a project aimed at reducing ample time passengers spend on bus stations while waiting for buses to arrive. I Worked with a team of 4 developers to create Phantom Project's chores and features for both backend and frontend. Personally Implemented chore project set up which included pipeline and Code climate for code optimization, database migration, code linting. Worked on different features which include registration, and CRUD of different operations. Reviewed different PRs from my teammates and tested different features using Mocha and Chai",
+      "This is a project aimed at reducing ample time passengers spend on bus stations while waiting for buses to arrive. I Worked with a team of 4 developers to create Phantom Project's chores and features for both backend and frontend. Personally Implemented chore project set up which included pipeline and Code climate for code optimization, database migration, code linting. Worked on different features which include registration, and CRUD of different operations. Reviewed different PRs from my teammates and tested different features using Mocha and Chai"
   },
   {
     id: 2,
@@ -51,10 +54,27 @@ export const projects = [
     image2: projectmgmt2,
     link: "https://njohn-projectmgmt.netlify.app/",
     description:
-      "This is a management system web application that helps users to manage the projects that they are working on. A project is mapped to its client and the User(Individual Person or Company) are able to store different information for the client and the project as well as manipulate and use them in anyway helpful to them. I worked on this project alone and the challenges encountered where using a new server-side language which is an alternative to Restful APIs i.e GraphQL. I managed to learn a lot while working on the project and am using it to create more advanced applications which you will see soon on this page. ",
+      "This is a management system web application that helps users to manage the projects that they are working on. A project is mapped to its client and the User(Individual Person or Company) are able to store different information for the client and the project as well as manipulate and use them in anyway helpful to them. Some of the challenges I encountered while working on this project were using for the first time a new server-side language which is an alternative to Restful APIs i.e GraphQL. I managed to learn a lot while working on the project and am using it to create more advanced applications which you will see soon on this page. "
   },
   {
     id: 3,
+    position: "Frontend Developer | Personal",
+    name: "Chicken Order",
+    stacks: [
+      "Figma",
+      "JavaScript",
+      "ReactJs",
+      "React-bootstrap",
+      "Bootstrap CSS",
+    ],
+    image1: chicken1,
+    image2: chicken2,
+    link: "https://john-chicken-app.netlify.app/",
+    description:
+      "This is an application where by customers are capable of selecting the type of chicken dish they want. Their choice is added to the cart and they are able to manipulate the items in the cart and view them. I built this app while practicing the react Context API with useContext, useState, useEffect and custom hooks. The Context API is an advantage for React app to effectively produce global variables that can be passed around which would prevent prop-drilling.The skills and experience I got from this project can be used on a larger similar project in terms of stack or businesswise."
+  },
+  {
+    id: 4,
     position: "Full Stack Developer | Andela",
     name: "FlashCard",
     stacks: [
@@ -72,10 +92,10 @@ export const projects = [
     image2: flashcard2,
     link: "https://flashcard-levelup.netlify.app/",
     description:
-      "This is a project was created right after the above Management System App where I now wanted to use GraphQL and TypeScript to see how it would work together. I also used Prisma ORM for the first time and it was the easiest ORM I ever level up on so fast and was able to grasp well its concepts. This is a simple flashcard application for amusement and can be used by movie lovers to evaluate how well they know the movies they have ever watched. It has an incredible dashboard for all CRUD(Create, Read, Update and Delete) operations.",
+      "This is a project was created right after the above Management System App where I now wanted to use GraphQL and TypeScript to see how it would work together. I also used Prisma ORM for the first time and it was the easiest ORM I ever level up on so fast and was able to grasp well its concepts. This is a simple flashcard application for amusement and can be used by movie lovers to evaluate how well they know the movies they have ever watched. It has an incredible dashboard for all CRUD(Create, Read, Update and Delete) operations."
   },
   {
-    id: 4,
+    id: 5,
     position: "Full Stack | Personal",
     name: "Deprecated Personal Portfolio",
     stacks: [
@@ -92,10 +112,10 @@ export const projects = [
     image2: portfolio2,
     link: "https://njohndeprecated.netlify.com/",
     description:
-      "This project was my previous portfolio and obviously you can see there was a lot of improvement and new skills gained. When I was creating the new portfolio I imagined how coding can really feel tasty and challenging but afterall you get to learn a lot of new skills you can always imagine and appreciate yourself. The most challenge met on the journey to become a programmer I am today is Debugging Tensions. Debugging is a critical skill, but most people aren't born with a mastery of it. Debugging is frustrating. You just wrote a bunch of code, and it doesn't work even though you're pretty sure it should. I learnt a lot of tools that helped to reduce the amount of time spent while debugging and some of them even helped get rid of some bugs during the development and these include Browser Developer Tool, ESLint as well as using TypeScript which is awesome when it comes to type-checking and prevents one from encounting bugs during the runtime.",
+      "This project was my previous portfolio and obviously you can see there was a lot of improvement and new skills gained. When I was creating the new portfolio I imagined how coding can really feel tasty and challenging but afterall you get to learn a lot of new skills you can always imagine and appreciate yourself. The whole journey to reach the stage I am wasn't easy, among the challenges met, there is Debugging Tensions. Debugging is a critical skill, but most people aren't born with a mastery of it. Debugging is frustrating. You just wrote a bunch of code, and it doesn't work even though you're pretty sure it should. I learnt a lot of tools that helped to reduce the amount of time spent while debugging and some of them even helped get rid of some bugs during the development and these include Browser Developer Tool, ESLint as well as using TypeScript which is awesome when it comes to type-checking and prevents one from encounting bugs during the runtime."
   },
   {
-    id: 5,
+    id: 6,
     position: "Frontend Developer | Personal",
     name: "epicLib - In progress",
     stacks: ["Figma", "JavaScript", "HTML", "Tailwindcss"],
@@ -103,6 +123,6 @@ export const projects = [
     image2: epiclib2,
     link: "https://github.com/niyontwali/epicLib",
     description:
-      "This application is in progress and it's main object will be helping beginner to learn and get equiped with the need skills related to JavaScript and most of its frameworks/libraries like Reactjs, Nextjs for the frontend and Nodejs for the backend. This web application will be providing free courses and resources that will be posted on a regulary basis to help the interested end users. I am currently done with the frontend and starting the backend which will be complete soon and the application will be live soon.",
+      "This application is in progress and it's main object will be helping beginner to learn and get equiped with the need skills related to JavaScript and most of its frameworks/libraries like Reactjs, Nextjs for the frontend and Nodejs for the backend. This web application will be providing free courses and resources that will be posted on a regulary basis to help the interested end users. I am currently done with the frontend and starting the backend which will be complete soon and the application will be live soon."
   },
 ];
